0a53ba9eedbe9329b850b0155f18f7692ee6e5fc
zhanghao <zhanghao1903@qq.com>
2023-10-26 16:51:12 +0800

    个人自用脚本

--- baidu-bes/log_count.py
+++ baidu-bes/log_count.py
@@ -1,15 +1,32 @@
 import os
+import sys
 
+file_path = './rtb_behavior.20231020_18.log'
+# 获取第一个命令行参数
+if len(sys.argv) > 1:
+    file_path = file_path + sys.argv[1]
 
-file_path = '/Users/zhanghao/PycharmProjects/pythonProject/baidu-bes/rta_behavior.20231017_00.log'
 log_count_map = {}
+
+def add_to_dict(record):
+    global log_count_map
+    print(record[4], record[5])
+    print(f'price={record[19]}, cost={record[20]}, type={record[21]}')
+    action = record[0].split()[-1]
+    media = record[2]
+    key = media + '-' +  action
+    if key in log_count_map:
+        log_count_map[key] += 1
+    else:
+        log_count_map[key] = 1
+
+
 if not os.path.exists(file_path):
     print(f"文件{file_path}不存在")
 with open(file_path, 'r') as log_file:
     for line in log_file:
         record = line.split("\t")
-        print(record)
+        add_to_dict(record)
 
 
-def add_to_dict(record):
-    global log_count_map
+print(log_count_map)
--- request.py
+++ request.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 secret_key = 'JniWQjitRfQZntCH'
 domain_name = 'https://api.e.mi.com'
@@ -9,32 +10,20 @@ update_url = '/openapi/tools/userPackage/update/tag'  # 请替换为实际的上
 update_list_url = '/openapi/tools/userPackage/updateInfo/availableUpdateList'
 
 state_url = '/openapi/tools/userPackage/updateInfo/get'
-# 要上传的文件路径
-file_path = '/data/apps/rta-package-upload/jd/20231008/oaidOrigin_only_1173456292.txt-xiaomi-tmp/oaidOrigin_only_1173456292.txt.split.2.txt'  # 请替换为实际的文件路径
-
-# 请求参数
-params = {
-    'customerId': 482232,  # 第一个参数
-    'model': 'overwrite',
-    'signId': 'b741ef165103518ef3fe23a3aeaea071',
-    'tagId': 281811,   # 第二个参数
-}
 
 
-available_params = {
-    'customerId': 482232,  # 第一个参数
-    'signId': 'b741ef165103518ef3fe23a3aeaea071',
-    # 'tagId': 273003,
-  # 第二个参数
-}
-
-state_params = {
-    'customerId': 475802,  # 第一个参数
-    'signId': 'b741ef165103518ef3fe23a3aeaea071',
-    'tagId': 273018,
-  # 第二个参数
-}
-
+def build_param(customer_id, tag_id):
+    # 请求参数
+    params = {
+        'customerId': customer_id,  # 第一个参数
+        # 'model': 'overwrite',
+        'signId': 'b741ef165103518ef3fe23a3aeaea071',
+        'tagId': tag_id,   # 第二个参数
+    }
+    params['sign'] = get_str(params)
+    # print(available_params['sign'])
+    # 使用字典构建 formData
+    return params
 
 
 def get_str(param_map):
@@ -56,34 +45,27 @@ def get_md5(base_str):
     return md5_hash.hexdigest().lower()
 
 
-params['sign'] = get_str(params)
-print(params['sign'])
-available_params['sign'] = get_str(available_params)
-print(available_params['sign'])
-# 使用字典构建 formData
-# form_data = {
-#     'targetFile': (file_path, open(file_path, 'rb'))  # 将文件添加到 formData
-# }
-
-
-def post_req(url, params, files):
-    response = requests.post(domain_name + url, files=form_data, params=params)
-
+def post_req(params, file_path):
+    form_data = {
+        'targetFile': (file_path, open(file_path, 'rb'))  # 将文件添加到 formData
+    }
+    response = requests.post(domain_name + update_url, files=form_data, params=params)
     response.encoding = 'utf-8'
     # 打印响应
     print(response.status_code)
     print(response.text)
 
 
-def get_req(url, params):
-    response = requests.get(url=domain_name + update_list_url, params=available_params)
+def get_req(customer_id, tag_id):
+    available_params = build_param(customer_id, tag_id)
+    response = requests.get(url=domain_name + state_url, params=available_params)
     response.encoding = 'utf-8'
     # 打印响应
     print(response.status_code)
     print(response.text)
 
 
-get_req(update_list_url, available_params)
-#get_req(state_url, state_params)
-# post_req(update_url, params, form_data)
-#get_req(state_url, state_params)
+customer_id = 482192
+tag_id = 285600
+get_req(customer_id, tag_id)
+param = build_param(customer_id, tag_id)
